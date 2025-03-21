6b1bfa2c4cbecbe626fdddf054ef6f536e395ce4
Mohamed Amin <itp.coder@gmail.com>
2025-03-21 16:26:59 +0000

    ok

--- React_Lawyer/React_Lawyer.Server/Program.cs
+++ React_Lawyer/React_Lawyer.Server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using React_Lawyer.Server.Data;
 
 namespace React_Lawyer.Server
 {
@@ -10,6 +12,12 @@ namespace React_Lawyer.Server
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
