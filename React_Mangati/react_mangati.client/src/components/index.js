0652a35f9996724de770a02aa1bbbc41de1c7947
Mohamed Amin <itp.coder@gmail.com>
2025-05-23 20:52:36 +0100

    ok

---
+++ React_Mangati/react_mangati.client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as MainLayout } from './Layout/MainLayout/MainLayout';
+export { default as Header } from './Layout/Header/Header';
+export { default as Footer } from './Layout/Footer/Footer';
+export { default as Sidebar } from './Layout/Sidebar/Sidebar';
